42ffd3ba182212c81c2121585b264e4828226d92
rferrei3 <rubens.ferreira@alticeusa.com>
2023-05-31 17:47:24 -0300

    corrected keymaps for auto open-close brackets

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,12 +1,14 @@
 if vim.fn.has("mac") > 0 then
     vim.keymap.set("n", "<C-/>", "<Plug>NERDCommenterToggle")
     vim.keymap.set("v", "<C-/>", "<Plug>NERDCommenterToggle<CR>gv")
-
-    vim.keymap.set("v", "<leader>dd", "di''<ESC>hp")
 else
     vim.keymap.set("n", "<C-_>", "<Plug>NERDCommenterToggle")
     vim.keymap.set("v", "<C-_>", "<Plug>NERDCommenterToggle<CR>gv")
 end
+vim.keymap.set("v", "<leader>d'", "di''<ESC>hp")
+vim.keymap.set("v", "<leader>d(", "di()<ESC>hp")
+vim.keymap.set("v", "<leader>d[", "di[]<ESC>hp")
+vim.keymap.set("v", "<leader>d{", "di{}<ESC>hp")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
