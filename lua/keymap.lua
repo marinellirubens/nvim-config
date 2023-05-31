41a46bf2b91dc9e3e517dc868626e48dd7741a90
marinellirubens <marinelli.rubens@gmail.com>
2023-05-31 01:05:57 -0300

    corrected keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,7 +1,13 @@
 if vim.fn.has('macunix') then
     vim.keymap.set("n", "<C-/>", "<Plug>NERDCommenterToggle")
     vim.keymap.set("v", "<C-/>", "<Plug>NERDCommenterToggle<CR>gv")
+
+    vim.keymap.set("v", "<leader>dd", "di''<ESC>hp")
 else
     vim.keymap.set("n", "<C-_>", "<Plug>NERDCommenterToggle")
     vim.keymap.set("v", "<C-_>", "<Plug>NERDCommenterToggle<CR>gv")
-end 
+end
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
