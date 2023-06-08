fc7e9f481e057e8190e1c563081846862fcb0109
rferrei3 <rubens.ferreira@alticeusa.com>
2023-06-08 13:51:00 -0300

    inclued ctrl backspace configuration

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -15,3 +15,4 @@ vim.keymap.set("v", "<leader>d{", "di{}<ESC>hp")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("i", "<C-H>", "<C-W>")
