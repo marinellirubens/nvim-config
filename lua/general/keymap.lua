7f1e77a06f4e9c511595a746ea709064c7db16f6
marinellirubens <marinelli.rubens@gmail.com>
2024-01-04 16:18:30 -0300

    Included nromap and silent on keymap

--- lua/general/keymap.lua
+++ lua/general/keymap.lua
@@ -7,7 +7,7 @@ else
     vim.keymap.set("n", "<C-_>", "<Plug>NERDCommenterToggle")
     vim.keymap.set("v", "<C-_>", "<Plug>NERDCommenterToggle<CR>gv")
 end
-vim.keymap.set("n", "<leader>q",":nohlsearch<Bar>:echo<CR>")
+vim.keymap.set("n", "<leader>q",":nohlsearch<Bar>:echo<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>","<C-d>zz")
 vim.keymap.set("v", "<C-j>","<C-d>zz")
 vim.keymap.set("n", "<C-k>","<C-u>zz")
@@ -17,12 +17,11 @@ vim.keymap.set("v", "<C-d>","<C-d>zz")
 vim.keymap.set("n", "<C-u>","<C-u>zz")
 vim.keymap.set("v", "<C-u>","<C-u>zz")
 
-vim.keymap.set("n", "<f2>",":NvimTreeOpen<CR>:NvimTreeToggle<CR>")
-vim.keymap.set("n", "<f3>",":NvimTreeRefresh<CR>")
-vim.keymap.set("n", "<f2>",":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<f2>",":NvimTreeOpen<CR>:NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<f3>",":NvimTreeRefresh<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<f2>",":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<f2>",":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>ps", ":lua require'telescope.builtin'.grep_string({ search = vim.fn.input(\"Grep for >\")})<CR>")
+vim.keymap.set("n", "<leader>ps", ":lua require'telescope.builtin'.grep_string({ search = vim.fn.input(\"Grep for >\")})<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
@@ -40,9 +39,9 @@ vim.keymap.set("n", "<C-w><C-k>", "<cmd>horizontal resize -5<cr>")
 vim.keymap.set("n", "<leader>r", "<cmd>source ~/.config/nvim/init.vim<cr>")
 vim.keymap.set("n", "<leader>tp", "<cmd>TransparentToggle<cr>")
 
-vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
-vim.keymap.set("n", "<leader>c", ":set colorcolumn=100<CR>")
-vim.keymap.set("n", "<leader>nc", ":set colorcolumn-=100<CR>")
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", ":set colorcolumn=100<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>nc", ":set colorcolumn-=100<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "<leader>d'", "di''<ESC>hp")
 vim.keymap.set("v", '<leader>d"', 'di""<ESC>hp')
@@ -55,7 +54,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("i", "<C-H>", "<C-W>")
-vim.keymap.set("n", "<leader>nd", ":Telescope diagnostics<CR>")
+vim.keymap.set("n", "<leader>nd", ":Telescope diagnostics<CR>", { noremap = true, silent = true })
 
 --implement this later
 --https://www.reddit.com/r/neovim/comments/mvhfw7/comment/gvd8rb9/?utm_source=share&utm_medium=web2x&context=3
