1fa1d7f80eb88d29a7d4584445c0010f2fa0d957
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-19 15:48:02 -0300

    Included tag and markdown on ignore list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    tag = 'v0.9.1',
     config = function()
         require('nvim-treesitter.configs').setup {
           -- A list of parser names, or "all" (the five listed parsers should always be installed)
@@ -13,7 +14,7 @@ return {
           auto_install = true,
 
           -- List of parsers to ignore installing (for "all")
-          ignore_install = { "javascript" },
+          ignore_install = { "javascript", "markdown" },
 
           ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
           -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
