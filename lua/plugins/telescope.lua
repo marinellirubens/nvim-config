e4523c08e6483b7c8f51c8e83122f097a268617f
rferrei3 <rubens.ferreira@alticeusa.com>
2024-01-21 13:09:11 -0300

    Changed configuration of find files on telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,21 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+        require("telescope").setup {
+            defaults = {
+                file_ignore_patterns = {
+                    '.git/',
+                    '__pycache__/',
+                    'node_modules/',
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = true
+                },
+            },
+        }
+    end
 }
