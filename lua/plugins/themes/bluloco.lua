8dff79c4a4240797a3d3f1987a86aaaf7e879ff4
marinellirubens <marinelli.rubens@gmail.com>
2024-01-29 02:41:57 -0300

    Saving theme changes

--- lua/plugins/themes/bluloco.lua
+++ lua/plugins/themes/bluloco.lua
@@ -1,7 +1,5 @@
 return {
   'uloco/bluloco.nvim',
-  lazy = false,
-  priority = 1000,
   dependencies = { 'rktjmp/lush.nvim' },
   config = function()
     -- your optional config goes here, see below.
--- lua/plugins/themes/catppuccin.lua
+++ lua/plugins/themes/catppuccin.lua
@@ -1,8 +1,6 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = false,
-    priority = 1000,
     config = function()
         require("catppuccin").setup({
             flavour = "mocha", -- latte, frappe, macchiato, mocha
