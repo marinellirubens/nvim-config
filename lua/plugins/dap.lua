f876ab40594ee9256042a00675713a4275a08324
rferrei3 <rubens.ferreira@alticeusa.com>
2024-04-04 10:34:37 -0300

    Included nvim-dap dependency

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,7 +2,7 @@ return {
     {
         'rcarriga/nvim-dap-ui',
         ft = vim.g.languages,
-        dependencies = {'mfussenegger/nvim-dap'},
+        dependencies = {'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio'},
         config = function()
             require("dapui").setup()
 
