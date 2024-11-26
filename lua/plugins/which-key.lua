0b1af836c1bce4ac4aa5254db82b37efe3e406c8
marinellirubens <marinelli.rubens@gmail.com>
2024-11-26 12:36:58 -0300

    corrected whic-key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,35 +4,35 @@ local opts = {
         local wk = require("which-key")
         --local map = vim.keymap.set
         local mappings = {
-            { "rn", vim.lsp.buf.rename, "[R]e[n]ame" },
-            { "ca", vim.lsp.buf.code_action, "[C]ode [A]ction" },
-            { "wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder" },
-            { "wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder" },
+            { "rn", vim.lsp.buf.rename, desc="[R]e[n]ame" },
+            { "ca", vim.lsp.buf.code_action, desc="[C]ode [A]ction" },
+            { "wa", vim.lsp.buf.add_workspace_folder, desc="[W]orkspace [A]dd Folder" },
+            { "wr", vim.lsp.buf.remove_workspace_folder, desc="[W]orkspace [R]emove Folder" },
             { "wl",
                 function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end,
-                "[W]orkspace [L]ist Folders",
+                desc="[W]orkspace [L]ist Folders",
             },
-            { "D", vim.lsp.buf.type_definition, "Type [D]efinition" },
-            { "ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols" },
-            { "ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols" },
+            { "D", vim.lsp.buf.type_definition, desc="Type [D]efinition" },
+            { "ds", require("telescope.builtin").lsp_document_symbols, desc="[D]ocument [S]ymbols" },
+            { "ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, desc="[W]orkspace [S]ymbols" },
             { "e",
                 function()
                     vim.diagnostic.open_float()
                 end,
-                "",
+                desc="",
             },
-            { "ne", vim.diagnostic.goto_next, "[N]ext [E]rror" },
+            { "ne", vim.diagnostic.goto_next, desc="[N]ext [E]rror" },
         }
         wk.add(mappings, { prefix = "<leader>" })
 
         local mappings2 = {
-            { "gd", vim.lsp.buf.definition, "[G]oto [D]efinition" },
-            { "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences" },
-            { "gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation" },
-            { "K", vim.lsp.buf.hover, "Hover Documentation" },
-            { "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration" },
+            { "gd", vim.lsp.buf.definition, desc="[G]oto [D]efinition" },
+            { "gr", require("telescope.builtin").lsp_references, desc="[G]oto [R]eferences" },
+            { "gI", vim.lsp.buf.implementation, desc="[G]oto [I]mplementation" },
+            { "K", vim.lsp.buf.hover, desc="Hover Documentation" },
+            { "gD", vim.lsp.buf.declaration, desc="[G]oto [D]eclaration" },
         }
         wk.add(mappings2)
         wk.setup()
