f7efce426ec412e3a6be610d0f145981f9bee93a
marinellirubens <marinelli.rubens@gmail.com>
2025-02-02 22:24:21 -0300

    Changes for better usage

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,5 @@
 local opts = {
     --plugin to render markdown on browser (requires lib grip installed via python [pip install grip] )
-    { 'PratikBhusal/vim-grip' },
     {
         --markdown syntax highlight
         "preservim/vim-markdown",
