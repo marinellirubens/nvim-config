67b8f20bd18d390b73f67895bd154dd1e0ff0a6d
marinellirubens <marinelli.rubens@gmail.com>
2026-07-02 01:26:24 -0300

    changed some stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,19 @@
 local opts = {
     "nvim-treesitter/nvim-treesitter",
-    tag = "v0.9.1",
+    branch = "main",
     ft = vim.g.languages,
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "python", "lua", "vim", "vimdoc", "query" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = true,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = false,
-
-            -- List of parsers to ignore installing (for "all")
-            ignore_install = { "javascript", "markdown" },
-
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            --parser_install_dir = "~/.config/treesitter/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-            indent = {
-                enable = false,
-                disable = { "python" },
-            },
-            highlight = {
-                enable = true,
-                disable = {},
-                additional_vim_regex_highlighting = { "python" },
-            },
-        })
+        require("nvim-treesitter").setup()
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
 }
 
