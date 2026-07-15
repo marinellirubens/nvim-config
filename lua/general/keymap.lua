3983ae00a7cb42c075f44efb2864c7aed48e32ed
marinellirubens <marinelli.rubens@gmail.com>
2026-07-15 16:41:18 -0300

    corrected description on telescope keybind

--- lua/general/keymap.lua
+++ lua/general/keymap.lua
@@ -48,7 +48,7 @@ vim.keymap.set("n", "<leader>fo", "<cmd>Telescope buffers<cr>", { noremap = true
 vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { noremap = true, silent = true , desc = "Telescope keymaps"})
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true , desc = "Telescope help tags"})
 vim.keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", { noremap = true, silent = true , desc = "Telescope help tags"})
-vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { noremap = true, silent = true , desc = "Telescope help tags"})
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { noremap = true, silent = true , desc = "Telescope resume last search"})
 vim.keymap.set("n", "<leader>fgs", "<cmd>Telescope git_status<cr>", { noremap = true, silent = true , desc = "Telescope help tags"})
 vim.keymap.set("n", "<leader>fgc", "<cmd>Telescope git_commits<cr>", { noremap = true, silent = true , desc = "Telescope git commits (all)"})
 vim.keymap.set("n", "<leader>fgb", "<cmd>Telescope git_bcommits<cr>", { noremap = true, silent = true , desc = "Telescope git commits (branch)"})
